6e5a482f2f21bc82fbf0440e70c36184a71074ae
Juan Arturo <cripto1989@gmail.com>
2018-10-10 11:22:51 -0500

    It adds init dict example script

---
+++ dict_init.py
@@ -0,0 +1,20 @@
+"""
+3 ways to init a dict
+"""
+
+# Using curly braces directly
+music = {
+    'title': 'I started the joke',
+    'author' 'Bee Gees'
+    'duration_seconds': 350
+}
+
+# Using dict constructor and tuples
+song = dict([
+    ("title","It's my life"),
+    ("singer", "Dr. Alban"),
+    ("duration_seconds", 350)
+])
+
+# Using dict constructor and only params
+last = dict(title='It\'s my life', singer='Dr Alban', duration_seconds=350)
